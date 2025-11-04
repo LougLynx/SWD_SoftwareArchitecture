189a07b986055931dee8cf8fbf5e9fde0ce7eed2
LougLynx <longtd204@gmail.com>
2025-11-04 21:48:19 +0700

    Add project files.

---
+++ SWD_SoftwareArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWD_SoftwareArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
